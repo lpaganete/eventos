507fa14a72096a237bde23529e8e7efb7b7ac2f7
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-10-08 10:28:20 -0300

    Deixando menu dinâmico

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+
 
 function Navbar() {
+
+    const dispatch = useDispatch();
+
     return (
         <nav className="navbar navbar-expand-lg">
             <span className="navbar-brand text-white font-weight-bold">Eventos</span>
@@ -15,12 +20,29 @@ function Navbar() {
                     <li className="nav-item">
                         <Link className="nav-link" to="/">Home</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/novousuario">Cadastrar</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/login">Login</Link>
-                    </li>
+                    {
+                        useSelector(state => state.usuarioLogado) > 0 ?
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="">Meus Eventos</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="">Publicar Evento</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" onClick={() => dispatch({ type: 'LOG_OUT' })}>Sair</Link>
+                                </li>
+                            </>
+                            :
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/novousuario">Cadastrar</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                            </>
+                    }
                 </ul>
             </div>
         </nav>
