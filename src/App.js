094a6c56306c67d3066e90d6b8a2504569c7c0f7
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-10-15 10:14:36 -0300

    Componente para Rotas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import store from '../src/store/';
-import { Provider } from 'react-redux';
-
-/* PAGINAS */
-import Login from './view/login';
-import NovoUsuario from './view/usuario-novo';
-import Home from './view/home';
-import UsuarioRecuperarSenha from './view/usuario-recuperar-senha';
-import EventoCadastro from './view/evento-cadastro';
+import Routes from './routes';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/novousuario' component={NovoUsuario} />
-        <Route exact path='/usuariorecuperarsenha' component={UsuarioRecuperarSenha} />
-        <Route exact path='/eventocadastro' component={EventoCadastro} />
-      </Router>
-    </Provider>
+    <Routes />
   );
 }
 
