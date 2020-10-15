755179eb3de8724c2da5b399590f67682dea66b3
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-10-15 10:08:23 -0300

    Implementando pesquisa de eventos por título

--- src/components/evento-card/index.js
+++ src/components/evento-card/index.js
@@ -10,7 +10,7 @@ function EventoCard({ id, img, titulo, detalhes, visualizacoes }) {
 
     useEffect(() => {
         firebase.storage().ref(`imagens/${img}`).getDownloadURL().then(url => setUrlImagem(url));
-    });
+    }, [urlImagem]);
 
     return (
 
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -11,25 +11,30 @@ import EventoCard from '../../components/evento-card/';
 function Home() {
 
     const [eventos, setEventos] = useState([]);
+    const [pesquisa, setPesquisa] = useState('');
+
     let listaEventos = [];
 
     useEffect(() => {
         firebase.firestore().collection('eventos').get().then(async (resultado) => {
             await resultado.docs.forEach(doc => {
-                listaEventos.push({
-                    id: doc.id,
-                    ...doc.data()
-                })
-            })
-
+                if (doc.data().titulo.indexOf(pesquisa) >= 0) {
+                    listaEventos.push({
+                        id: doc.id,
+                        ...doc.data()
+                    });
+                }
+            });
             setEventos(listaEventos);
-
-        })
+        });
     });
 
     return (
         <>
             <Navbar />
+            <div className="row p-5">
+                <input onChange={(e) => setPesquisa(e.target.value)} type="text" className="form-control text-center" placeholder="Pesquisar evento pelo título..." />
+            </div>
 
             <div className="row p-3">
                 {eventos.map(item => <EventoCard key={item.id} id={item.id} img={item.foto} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes} />)}
